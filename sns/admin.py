d2218a89cc33cab7dd438110df948ea1c49127cd
saka-21 <kasaikasai572@gmail.com>
2020-06-08 23:55:14 +0900

    Add: ログイン機能作成途中

--- sns/admin.py
+++ sns/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Post, Category
+from django.contrib.auth.admin import UserAdmin
+from .models import Post, Category, CustomUser
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age',)}),)
+    list_display = ['username', 'email', 'age']
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(CustomUser, CustomUserAdmin)
--- sns/models.py
+++ sns/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    age = models.IntegerField(null=True)
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
