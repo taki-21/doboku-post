d6f62045e7fef0f419155692de5d58714066e487
saka-21 <kasaikasai572@gmail.com>
2020-06-08 21:37:35 +0900

    Add: カテゴリーリストの追加

--- sns/models.py
+++ sns/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
@@ -16,6 +17,7 @@ class Post(models.Model):
     content = models.TextField()
     published_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='')
+
 
     class Meta:
         ordering = ['-published_at']
