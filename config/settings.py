445a5c348cbd7e515276a275e10eae7cf5cc97d7
saka-21 <kasaikasai572@gmail.com>
2020-06-09 19:28:12 +0900

    Mod: admin.pyの修正

--- config/settings.py
+++ config/settings.py
@@ -127,4 +127,3 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-AUTH_USER_MODEL = 'sns.CustomUser'
--- sns/admin.py
+++ sns/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Post, Category, CustomUser
+from .models import Post, Category
 # Register your models here.
 
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age',)}),)
-    list_display = ['username', 'email', 'age']
 
 admin.site.register(Post)
 admin.site.register(Category)
-admin.site.register(CustomUser, CustomUserAdmin)
