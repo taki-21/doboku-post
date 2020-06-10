486cb0c9fdc0a065aea39156cacb0bb67bcde3f1
saka-21 <kasaikasai572@gmail.com>
2020-06-10 19:15:13 +0900

    Add: デモ投稿データ追加

--- sns/models.py
+++ sns/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Category(models.Model):
@@ -12,7 +13,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
-    auther = models.CharField(max_length=100)
+    auther = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_at = models.DateTimeField(auto_now_add=True)
