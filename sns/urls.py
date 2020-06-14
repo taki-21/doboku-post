35c9114ef689c1e11d16f1b47f6a5964bc548e48
saka-21 <kasaikasai572@gmail.com>
2020-06-14 20:07:21 +0900

    Add: カテゴリー別投稿一覧画面にもソート機能を追加#2
    Mod: CategoryPostViewのメソッドの修正#2

--- sns/urls.py
+++ sns/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'sns'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:condition>/', views.IndexView.as_view(), name='index_condition'),
+    path('<int:condition>/', views.IndexView.as_view(), name='index'),
     path('detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('category/<str:category_slug>/',
          views.CategoryPostView.as_view(), name='category_post'),
     path('category/<str:category_slug>/<int:condition>',
-         views.CategoryPostView.as_view(), name='category_post_condition'),
+         views.CategoryPostView.as_view(), name='category_post'),
     path('create/', views.form_view, name='form_view'),
     path('mypage/', views.MyPage.as_view(), name='my_page')
 ]
