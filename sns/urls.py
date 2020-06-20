b1a0db9688fbf1fed5728438d58f6e423fbae111
saka-21 <kasaikasai572@gmail.com>
2020-06-20 22:09:03 +0900

    Add: コメント削除機能追加#7

--- sns/urls.py
+++ sns/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('my_page/<int:pk>/', views.MyPage.as_view(), name='my_page'),
     path('comment/<int:post_pk>/', views.comment_create, name='comment_create'),
     path('reply/<int:comment_pk>/', views.reply_create, name='reply_create'),
+    path('comment/<int:comment_pk>/remove/', views.comment_remove, name='comment_remove'),
     path('good/<int:pk>/', views.good_func, name='good'),
 ]
