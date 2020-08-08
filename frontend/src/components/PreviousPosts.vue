466535f15b8d64976a5ccf43e142a29b5845c81c
saka-21 <kasaikasai572@gmail.com>
2020-08-08 20:59:24 +0900

    Update: これまでの投稿のレイアウト調整

--- frontend/src/components/PreviousPosts.vue
+++ frontend/src/components/PreviousPosts.vue
@@ -1,72 +1,80 @@
 <template>
-  <div
-    class="uk-grid-column-small uk-child-width-1-3@l uk-child-width-1-2@m uk-child-width-1-1@s uk-text-center"
-    uk-grid
-  >
-    <router-link
-      class="router-link"
-      :to="{name: 'detail', params:{id: post.id }}"
-      v-for="(post, key) in previousPosts"
-      :key="key"
+  <div>
+    <div
+      class="uk-grid-column-small uk-child-width-1-3@l uk-child-width-1-2@m uk-child-width-1-1@s uk-text-center"
+      uk-grid
     >
-      <div class="uk-card uk-card-hover uk-card-default" id="card">
-        <div class="uk-card-media-top">
-          <img :src="post.image_change" />
-        </div>
-        <div class="uk-card-body">
-          <div class="uk-comment-header uk-position-relative">
-            <div>
-              <router-link
-                class="show_user"
-                :to="{name: 'mypage', params:{user_id: post.author.id}}"
-              >
-                <div>
-                  <img class="user_icon" v-bind:src="post.author.icon_image" />
-                  <span class="uk-comment-title uk-margin-remove">{{ post.author.username }}</span>
-                </div>
-              </router-link>
-              <div class="timestamp">
-                <span>{{ post.published_at | moment }}</span>
-              </div>
-            </div>
+      <router-link
+        class="router-link"
+        :to="{name: 'detail', params:{id: post.id }}"
+        v-for="(post, key) in previousPosts"
+        :key="key"
+      >
+        <div class="uk-card uk-card-hover uk-card-default" id="card">
+          <div class="uk-card-media-top">
+            <img :src="post.image_change" />
           </div>
-          <strong>{{ post.title }}</strong>
-          <p class="post_content">
-            <span>--</span>
-            {{ post.content }}
-          </p>
-          <div v-if="user_id == auth_id">
-            <div>
-              <router-link :to="{name: 'post_edit', params:{post_id: post.id }}">
-                <i uk-icon="icon: pencil"></i>
-              </router-link>
-               <!-- @click="DestroyPost(post.id)"" -->
-              <a :href="'#modal-' + post.id" uk-toggle>
-                <i uk-icon="icon: trash"></i>
-              </a>
-              <div :id="'modal-' + post.id" uk-modal>
-                <div class="uk-modal-dialog uk-modal-body">
-                  <h2 class="uk-modal-title">削除確認</h2>
-                  <p>投稿：{{ post.title }}を削除します。よろしいですか？</p>
-                  <p class="uk-text-right">
-                    <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
-                    <button class="uk-button uk-button-primary uk-modal-close" type="button" @click="DestroyPost(post.id)">OK
-                    </button>
-                  </p>
+          <div class="uk-card-body">
+            <div class="uk-comment-header uk-position-relative">
+              <div>
+                <router-link
+                  class="show_user"
+                  :to="{name: 'mypage', params:{user_id: post.author.id}}"
+                >
+                  <div>
+                    <img class="user_icon" v-bind:src="post.author.icon_image" />
+                    <span class="uk-comment-title uk-margin-remove">{{ post.author.username }}</span>
+                  </div>
+                </router-link>
+                <div class="timestamp">
+                  <span>{{ post.published_at | moment }}</span>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="comment_like_icon">
-            <i id="heart-button" uk-icon="comment"></i>
-            <span id="comment-count"></span>
-            <i id="heart-button" uk-icon="heart"></i>
-            <span id="like-count">{{ post.likes_count }}</span>
+            <strong>{{ post.title }}</strong>
+            <p class="post_content">
+              <span>--</span>
+              {{ post.content }}
+            </p>
+            <div v-if="user_id == auth_id">
+              <div>
+                <router-link :to="{name: 'post_edit', params:{post_id: post.id }}">
+                  <i uk-icon="icon: pencil"></i>
+                </router-link>
+                <!-- @click="DestroyPost(post.id)"" -->
+                <a :href="'#modal-' + post.id" uk-toggle>
+                  <i uk-icon="icon: trash"></i>
+                </a>
+                <div :id="'modal-' + post.id" uk-modal>
+                  <div class="uk-modal-dialog uk-modal-body">
+                    <h2 class="uk-modal-title">削除確認</h2>
+                    <p>投稿：{{ post.title }}を削除します。よろしいですか？</p>
+                    <p class="uk-text-right">
+                      <button
+                        class="uk-button uk-button-default uk-modal-close"
+                        type="button"
+                      >Cancel</button>
+                      <button
+                        class="uk-button uk-button-primary uk-modal-close"
+                        type="button"
+                        @click="DestroyPost(post.id)"
+                      >OK</button>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="comment_like_icon">
+              <i id="heart-button" uk-icon="comment"></i>
+              <span id="comment-count"></span>
+              <i id="heart-button" uk-icon="heart"></i>
+              <span id="like-count">{{ post.likes_count }}</span>
+            </div>
           </div>
         </div>
-      </div>
-      <!-- <pre>{{ posts }}</pre> -->
-    </router-link>
+        <!-- <pre>{{ posts }}</pre> -->
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -92,15 +100,17 @@ export default {
   },
   methods: {
     DestroyPost: function(post_id) {
-      api
-        .delete("/posts/" + post_id + "/")
-        .then(
-          this.$store.dispatch("post/getAllPosts"),
-          // ↓マイページにに飛ばしたいけどパラメータの付け方がわからない
-          this.$router.replace("/")
-          )
+      api.delete("/posts/" + post_id + "/").then(
+        this.$store.dispatch("post/getAllPosts"),
+        // ↓マイページにに飛ばしたいけどパラメータの付け方がわからない
+        this.$router.replace("/")
+      );
     }
   },
+  created() {
+    this.$store.dispatch("post/getAllPosts");
+  },
+
   filters: {
     moment: function(date) {
       return moment(date).format("YYYY/MM/DD HH:MM");
