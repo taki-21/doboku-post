58d6eaedb30c10b798eb3b759b24dc50e7974da8
saka-21 <kasaikasai572@gmail.com>
2020-09-05 01:39:55 +0900

    CIrcleCi設定ファイルの設定
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定
    
    CIrcleCi設定ファイルの設定

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
