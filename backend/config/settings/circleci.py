46395b5438420ad14e6c8a9fba0d247aa13cbe50
saka-21 <kasaikasai572@gmail.com>
2020-09-07 20:26:14 +0900

    Add: collectstatic

--- backend/config/settings/circleci.py
+++ backend/config/settings/circleci.py
@@ -140,10 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # メディアファイルの設定
 MEDIA_URL = '/media/'
