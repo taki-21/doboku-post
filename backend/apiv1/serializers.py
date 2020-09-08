338c92aebef6e82b2efd3a8235f72b45919f4fb5
saka-21 <kasaikasai572@gmail.com>
2020-09-08 22:56:09 +0900

    makemigrations

--- backend/apiv1/serializers.py
+++ backend/apiv1/serializers.py
@@ -40,7 +40,7 @@ class PostSerializer(serializers.ModelSerializer):
         queryset=get_user_model().objects.all(), write_only=True)
     likes_count = serializers.SerializerMethodField()
     comments_count = serializers.SerializerMethodField()
-    image_change = serializers.ImageField
+    image_change = serializers.ImageField()
     address = serializers.CharField(required=False)
     lat = serializers.DecimalField(
         required=False, max_digits=20, decimal_places=15,)
