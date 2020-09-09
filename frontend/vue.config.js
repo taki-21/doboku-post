582158eeb7286489b5e42f8d6695d40942d44993
saka-21 <kasaikasai572@gmail.com>
2020-09-09 23:32:01 +0900

    makemigrations

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,8 +8,8 @@ module.exports = {
   // outputDir: '../static',
   // indexPath: '../templates/index.html',
   // publicPath: process.env.NODE_ENV === 'production'
-  publicPath: '/',
-  productionSourceMap: false,
+  // publicPath: '/',
+  // productionSourceMap: false,
 
   //   ? '/static/'
   //   : '/'
