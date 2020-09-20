b9ce09142de9fd7d4c3bdbd559d2c47428452ec3
saka-21 <kasaikasai572@gmail.com>
2020-09-20 23:02:12 +0900

    Mod: product.settingsの修正#149

--- backend/apiv1/tests/test_views.py
+++ backend/apiv1/tests/test_views.py
@@ -85,6 +85,7 @@ class TestPostListCreateAPIView(APITestCase):
                 'T'),
             'comments_count': 0,
             'raw_image': None,
+            'image': None,
             'likes_count': 0,
             'prefecture': post.prefecture,
             'address': post.address,
