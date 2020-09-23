d358dcca7652f7791fb47f9e9dc88f7614e1e6b0
saka-21 <kasaikasai572@gmail.com>
2020-09-23 23:51:03 +0900

    新着投稿無限スクロールの実装#73

--- frontend/src/components/LatestPosts.vue
+++ frontend/src/components/LatestPosts.vue
@@ -22,50 +22,33 @@ export default {
     return {
       page: 1,
       latestPosts: [],
-      loading: false,
     };
   },
   methods: {
     infiniteHandler($state) {
-      //web.phpで設定したルーティング
       api
         .get("/posts/", {
           params: {
             page: this.page,
-            // per_page: 1,
           },
         })
         .then(({ data }) => {
           setTimeout(() => {
           if(data.results.length === 12){
-            console.log('おおおお')
             this.latestPosts.push(...data.results);
             this.page += 1;
             $state.loaded();
-
-            console.log('たたたた')
           }
           if (data.results.length < 12) {
             console.log("へいへい");
             this.latestPosts.push(...data.results);
-            // this.page += 1;
             $state.complete();
-            // $state.loaded();
           }}, 500)
-          // else {
-          // }
         })
         .catch(() => {
           $state.complete();
         });
     },
-  },
-  mounted() {
-    // this.infiniteHandler()
-    // api.get("/posts/").then((response) => {
-    //   this.latestPosts = response.data.results;
-    //   this.loading = false;
-    // });
   },
 };
 </script>
