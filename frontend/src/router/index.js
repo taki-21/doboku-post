cec618862d784567e905dae24dd5393dc3135f15
saka-21 <kasaikasai572@gmail.com>
2020-09-27 20:14:59 +0900

    Delete: 不要なvueファイル削除

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '@/views/Home'
 import LoginPage from '@/views/LoginPage'
 import store from '@/store'
 import HomePage from '@/views/HomePage'
