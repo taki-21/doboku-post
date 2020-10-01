9aa910cd566199a7a18e186b0db7efddf5987484
saka-21 <kasaikasai572@gmail.com>
2020-10-01 23:01:11 +0900

    Add: スクロール維持機能追加

--- frontend/src/components/LatestPosts.vue
+++ frontend/src/components/LatestPosts.vue
@@ -30,17 +30,27 @@ export default {
       latestPosts: [],
       loading: true,
       nextPage: false,
+      infinite: false,
     };
   },
   watch: {
     loading: function () {
       this.$nextTick(() => {
         var positionY = sessionStorage.getItem("positionY");
+        console.log(positionY);
         scrollTo(0, positionY);
         setTimeout(function () {
           scrollTo(0, positionY);
         }, 500);
       });
+    },
+    infinite() {
+      var positionY = sessionStorage.getItem("positionY");
+      console.log(positionY);
+      scrollTo(0, positionY);
+      setTimeout(function () {
+        scrollTo(0, positionY);
+      }, 500);
     },
   },
   mounted() {
@@ -75,6 +85,7 @@ export default {
               }
             }
           }, 500);
+          this.infinite = true;
         });
     },
   },
