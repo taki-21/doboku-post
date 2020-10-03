f6e124b9b915224b7724750315ba45a910f5acb6
saka-21 <kasaikasai572@gmail.com>
2020-10-03 18:48:43 +0900

    Add: sessionstorageの追加#169

--- frontend/src/components/LatestPosts.vue
+++ frontend/src/components/LatestPosts.vue
@@ -30,7 +30,7 @@ export default {
       latestPosts: [],
       loading: true,
       nextPage: false,
-      infinite: false,
+      // infinite: false,
       // infiniteId: 0,
     };
   },
@@ -39,7 +39,7 @@ export default {
     next();
   },
   watch: {
-    loading: function () {
+    loading() {
       this.$nextTick(() => {
         var positionY = sessionStorage.getItem("positionY");
         console.log(positionY);
@@ -49,25 +49,40 @@ export default {
         });
       });
     },
-    infinite() {
-      // if (this.infiniteId >= 2) {
-        var positionY = sessionStorage.getItem("positionY");
-        console.log(positionY);
-        // scrollTo(0, positionY);
-        setTimeout(function () {
-          scrollTo(0, positionY);
-        },500);
-      // }
-    },
+    // latestPosts() {
+    //   sessionStorage.setItem("posts", [this.latestPosts]);
+    // },
+    // infinite() {
+    //   // if (this.infiniteId >= 2) {
+    //     var positionY = sessionStorage.getItem("positionY");
+    //     console.log(positionY);
+    //     // scrollTo(0, positionY);
+    //     setTimeout(function () {
+    //       scrollTo(0, positionY);
+    //     },500);
+    //   // }
+    // },
   },
   mounted() {
-    api.get("/posts/").then((response) => {
-      this.latestPosts = response.data.results;
+    this.nextPage = JSON.parse(sessionStorage.getItem("nextpage"));
+    // sessionStorage.removeItem('posts')
+    // const session_posts = sessionStorage.getItem("posts");
+    if (sessionStorage.getItem("posts")) {
+      console.log("session");
+      this.latestPosts = JSON.parse(sessionStorage.getItem("posts"));
       this.loading = false;
-      if (response.data.next !== null) {
-        this.nextPage = true;
-      }
-    });
+    } else {
+      console.log("else");
+      api.get("/posts/").then((response) => {
+        this.latestPosts = response.data.results;
+        this.loading = false;
+        if (response.data.next !== null) {
+          this.nextPage = true;
+          sessionStorage.setItem("nextpage", JSON.stringify(this.nextPage));
+        }
+        sessionStorage.setItem("posts", JSON.stringify(this.latestPosts));
+      });
+    }
   },
   methods: {
     infiniteHandler($state) {
@@ -83,17 +98,30 @@ export default {
             // this.loading = false;
             if (data.results.length) {
               if (data.next === null) {
+                this.nextPage = false;
+                sessionStorage.setItem(
+                  "nextpage",
+                  JSON.stringify(this.nextPage)
+                );
                 this.latestPosts.push(...data.results);
+                sessionStorage.setItem(
+                  "posts",
+                  JSON.stringify(this.latestPosts)
+                );
                 $state.complete();
               } else {
                 this.latestPosts.push(...data.results);
+                sessionStorage.setItem(
+                  "posts",
+                  JSON.stringify(this.latestPosts)
+                );
                 this.page += 1;
                 $state.loaded();
               }
             }
           }, 500);
           // this.infiniteId++;
-          this.infinite = true;
+          // this.infinite = true;
         });
     },
   },
