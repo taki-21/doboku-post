68dfd37a290e4bd424d95fb22cdf1917981efba9
saka-21 <kasaikasai572@gmail.com>
2020-10-11 01:56:59 +0900

    Add: testコードの増強#128

--- backend/apiv1/tests/test_views.py
+++ backend/apiv1/tests/test_views.py
@@ -9,6 +9,8 @@ from unittest import TestCase
 TestCase.maxDiff = None
 
 
+# GET(正常系: 2, 異常系: 0)
+# POST(正常系: 1, 異常系: 1)
 class TestPostListCreateAPIView(APITestCase):
     """PostListCreateAPIViewのテストクラス"""
 
@@ -17,7 +19,7 @@ class TestPostListCreateAPIView(APITestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user = get_user_model().objects.create_user(
+        cls.user = get_user_model().objects.create(
             username="user",
             email='user@example.com',
             password="secret",
@@ -26,13 +28,142 @@ class TestPostListCreateAPIView(APITestCase):
             name='橋',
             slug='bridge'
         )
-        cls.category2 = Category.objects.create(
-            name='ダム',
-            slug='dam'
+        cls.post1 = Post.objects.create(
+            category=cls.category1,
+            author=cls.user,
+            title='飛騨トンネル',
+            content='あいうえお',
         )
 
+    def test_get_success(self):
+        """投稿モデルの取得（一覧）・投稿APIへのGETリクエスト(正常系)"""
+
+        # テストユーザーでログイン
+        token = str(RefreshToken.for_user(self.user).access_token)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+
+        # 投稿一覧をリクエスト
+        response = self.client.get(self.TARGET_URL)
+
+        # データベースの状態を検証
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(Category.objects.count(), 1)
+
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 200)
+
+        # 予想されるレスポンスを作成
+        post = Post.objects.get()
+        user = get_user_model().objects.filter(username='user').values()[0]
+        expected_json_dict = {
+            "count": 1,
+            "next": None,
+            "previous": None,
+            "results": [{
+                'id': post.id,
+                'category': post.category.id,
+                'author': {
+                    'date_joined': str(
+                        localtime(
+                            user['date_joined'])).replace(
+                        ' ',
+                        'T'),
+                    'email': 'user@example.com',
+                    'first_name': '',
+                    'groups': [],
+                    'icon_image': 'http://testserver/media/images/custom_user/icon_image/default_icon.png',
+                    'id': 1,
+                    'introduction': None,
+                    'is_active': True,
+                    'is_staff': False,
+                    'is_superuser': False,
+                    'last_login': None,
+                    'last_name': '',
+                    'password': user['password'],
+                    'user_permissions': [],
+                    'username': 'user'},
+                'title': post.title,
+                'content': post.content,
+                'published_at': str(
+                    localtime(
+                        post.published_at)).replace(
+                    ' ',
+                    'T'),
+                'comments_count': 0,
+                'raw_image': None,
+                'image': None,
+                'likes_count': 0,
+                'prefecture': post.prefecture,
+                'address': post.address,
+                'lat': post.lat,
+                'lng': post.lat,
+            }]}
+        self.assertJSONEqual(response.content, expected_json_dict)
+    def test_get_unauthorized_success(self):
+        """投稿モデルの取得（一覧）・投稿APIへのGETリクエスト(正常系)"""
+
+        # テストユーザーでログインしない
+
+        # 投稿一覧をリクエスト
+        response = self.client.get(self.TARGET_URL)
+
+        # データベースの状態を検証
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(Category.objects.count(), 1)
+
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 200)
+
+        # 予想されるレスポンスを作成
+        post = Post.objects.get()
+        user = get_user_model().objects.filter(username='user').values()[0]
+        expected_json_dict = {
+            "count": 1,
+            "next": None,
+            "previous": None,
+            "results": [{
+                'id': post.id,
+                'category': post.category.id,
+                'author': {
+                    'date_joined': str(
+                        localtime(
+                            user['date_joined'])).replace(
+                        ' ',
+                        'T'),
+                    'email': 'user@example.com',
+                    'first_name': '',
+                    'groups': [],
+                    'icon_image': 'http://testserver/media/images/custom_user/icon_image/default_icon.png',
+                    'id': 1,
+                    'introduction': None,
+                    'is_active': True,
+                    'is_staff': False,
+                    'is_superuser': False,
+                    'last_login': None,
+                    'last_name': '',
+                    'password': user['password'],
+                    'user_permissions': [],
+                    'username': 'user'},
+                'title': post.title,
+                'content': post.content,
+                'published_at': str(
+                    localtime(
+                        post.published_at)).replace(
+                    ' ',
+                    'T'),
+                'comments_count': 0,
+                'raw_image': None,
+                'image': None,
+                'likes_count': 0,
+                'prefecture': post.prefecture,
+                'address': post.address,
+                'lat': post.lat,
+                'lng': post.lat,
+            }]}
+        self.assertJSONEqual(response.content, expected_json_dict)
+
     def test_create_success(self):
-        """投稿モデルの登録APIへのPOSTリクエスト(正常系)"""
+        """投稿モデルの取得（一覧）・投稿APIへのPOSTリクエスト(正常系)"""
 
         # テストユーザーでログイン
         token = str(RefreshToken.for_user(self.user).access_token)
@@ -42,15 +173,15 @@ class TestPostListCreateAPIView(APITestCase):
         params = {
             'category': self.category1.id,
             'author_name': self.user.id,
-            'title': 'へのへのもへじ',
-            'content': 'あいうえおかきくけこ',
+            'title': '明石海峡大橋',
+            'content': 'かきくけこ',
         }
         response = self.client.post(self.TARGET_URL, params, format='json')
         # データベースの状態を検証
-        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(Post.objects.count(), 2)
         # レスポンスの内容を検証
         self.assertEqual(response.status_code, 201)
-        post = Post.objects.get()
+        post = Post.objects.get(id=2)
         user = get_user_model().objects.filter(username='user').values()[0]
         expected_json_dict = {
             'id': post.id,
@@ -64,7 +195,6 @@ class TestPostListCreateAPIView(APITestCase):
                 'email': 'user@example.com',
                 'first_name': '',
                 'groups': [],
-                'home_image': 'http://testserver/media/images/custom_user/home_image/home.png',
                 'icon_image': 'http://testserver/media/images/custom_user/icon_image/default_icon.png',
                 'id': 1,
                 'introduction': None,
@@ -95,7 +225,7 @@ class TestPostListCreateAPIView(APITestCase):
         self.assertJSONEqual(response.content, expected_json_dict)
 
     def test_create_bad_request(self):
-        """投稿モデルの登録APIへのPOSTリクエスト（異常系：バリデーションNG）"""
+        """投稿モデルの取得（一覧）・投稿APIへのPOSTリクエスト（異常系：バリデーションNG）"""
 
         # APIリクエストを実行
         params = {
@@ -107,6 +237,232 @@ class TestPostListCreateAPIView(APITestCase):
         response = self.client.post(self.TARGET_URL, params, format='json')
 
         # データベースの状態を検証
-        self.assertEqual(Post.objects.count(), 0)
+        self.assertEqual(Post.objects.count(), 1)
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 400)
+
+
+# GET(正常系: 2, 異常系: 0)
+# PATCH(正常系: 1, 異常系: 1)
+# DELETE(正常系: 0, 異常系: 0)
+class TestPostRetrieveUpdateDestroyAPIView(APITestCase):
+    """PostRetrieveUpdateDestroyAPIViewのテストクラス"""
+
+    TARGET_URL_WITH_PK = '/api/v1/posts/{}/'
+
+    @ classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = get_user_model().objects.create(
+            username="user",
+            email='user@example.com',
+            password="secret",
+        )
+        cls.category1 = Category.objects.create(
+            name='橋',
+            slug='bridge'
+        )
+        cls.post1 = Post.objects.create(
+            category=cls.category1,
+            author=cls.user,
+            title='へのへのもへじ',
+            content='あいうえおかきくけこ',
+        )
+
+    def test_get_success(self):
+        """投稿モデルの取得（詳細）・更新・削除APIへのGETリクエスト(正常系)"""
+
+        # テストユーザーでログイン(JWT認証)
+        token = str(RefreshToken.for_user(self.user).access_token)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+
+        # 投稿詳細をリクエスト
+        response = self.client.get(
+            self.TARGET_URL_WITH_PK.format(self.post1.id))
+        # データベースの状態を検証
+        # self.assertEqual(Post.objects.count(), 1)
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 200)
+        post = Post.objects.get()
+        user = get_user_model().objects.filter(username='user').values()[0]
+        expected_json_dict = {
+            'id': post.id,
+            'category': post.category.id,
+            'author': {
+                'date_joined': str(
+                    localtime(
+                        user['date_joined'])).replace(
+                    ' ',
+                    'T'),
+                'email': 'user@example.com',
+                'first_name': '',
+                'groups': [],
+                'icon_image': 'http://testserver/media/images/custom_user/icon_image/default_icon.png',
+                'id': 2,
+                'introduction': None,
+                'is_active': True,
+                'is_staff': False,
+                'is_superuser': False,
+                'last_login': None,
+                'last_name': '',
+                'password': user['password'],
+                'user_permissions': [],
+                'username': 'user'},
+            'title': post.title,
+            'content': post.content,
+            'published_at': str(
+                localtime(
+                    post.published_at)).replace(
+                ' ',
+                'T'),
+            'comments_count': 0,
+            'raw_image': None,
+            'image': None,
+            'likes_count': 0,
+            'prefecture': post.prefecture,
+            'address': post.address,
+            'lat': post.lat,
+            'lng': post.lat,
+        }
+        self.assertJSONEqual(response.content, expected_json_dict)
+
+    def test_get_unauthorized_success(self):
+        """投稿モデルの登録APIへのPOSTリクエスト（正常系：ログインしていないユーザーでも閲覧可能）"""
+
+        response = self.client.get(
+            self.TARGET_URL_WITH_PK.format(self.post1.id))
+        # データベースの状態を検証
+        # self.assertEqual(Post.objects.count(), 1)
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 200)
+        post = Post.objects.get()
+        user = get_user_model().objects.filter(username='user').values()[0]
+        expected_json_dict = {
+            'id': post.id,
+            'category': post.category.id,
+            'author': {
+                'date_joined': str(
+                    localtime(
+                        user['date_joined'])).replace(
+                    ' ',
+                    'T'),
+                'email': 'user@example.com',
+                'first_name': '',
+                'groups': [],
+                'icon_image': 'http://testserver/media/images/custom_user/icon_image/default_icon.png',
+                'id': 2,
+                'introduction': None,
+                'is_active': True,
+                'is_staff': False,
+                'is_superuser': False,
+                'last_login': None,
+                'last_name': '',
+                'password': user['password'],
+                'user_permissions': [],
+                'username': 'user'},
+            'title': post.title,
+            'content': post.content,
+            'published_at': str(
+                localtime(
+                    post.published_at)).replace(
+                ' ',
+                'T'),
+            'comments_count': 0,
+            'raw_image': None,
+            'image': None,
+            'likes_count': 0,
+            'prefecture': post.prefecture,
+            'address': post.address,
+            'lat': post.lat,
+            'lng': post.lat,
+        }
+        self.assertJSONEqual(response.content, expected_json_dict)
+
+    def test_patch_success(self):
+        """投稿モデルの取得（詳細）・更新・削除APIへのPATCHリクエスト(正常系)"""
+
+        # テストユーザーでログイン(JWT認証)
+        token = str(RefreshToken.for_user(self.user).access_token)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+
+        params = {
+            'id': self.post1.id,
+            'category': self.category1.id,
+            'author_name': self.user.id,
+            'title': 'あいうえお',
+            'content': 'かきくけこ',
+        }
+        response = self.client.patch(
+            self.TARGET_URL_WITH_PK.format(
+                self.post1.id), params, format='json')
+        # データベースの状態を検証
+        # self.assertEqual(Post.objects.count(), 1)
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 200)
+        post = Post.objects.get()
+        user = get_user_model().objects.filter(username='user').values()[0]
+        expected_json_dict = {
+            'id': post.id,
+            'category': post.category.id,
+            'author': {
+                'date_joined': str(
+                    localtime(
+                        user['date_joined'])).replace(
+                    ' ',
+                    'T'),
+                'email': 'user@example.com',
+                'first_name': '',
+                'groups': [],
+                'icon_image': 'http://testserver/media/images/custom_user/icon_image/default_icon.png',
+                'id': 2,
+                'introduction': None,
+                'is_active': True,
+                'is_staff': False,
+                'is_superuser': False,
+                'last_login': None,
+                'last_name': '',
+                'password': user['password'],
+                'user_permissions': [],
+                'username': 'user'},
+            'title': post.title,
+            'content': post.content,
+            'published_at': str(
+                localtime(
+                    post.published_at)).replace(
+                ' ',
+                'T'),
+            'comments_count': 0,
+            'raw_image': None,
+            'image': None,
+            'likes_count': 0,
+            'prefecture': post.prefecture,
+            'address': post.address,
+            'lat': post.lat,
+            'lng': post.lat,
+        }
+        self.assertJSONEqual(response.content, expected_json_dict)
+
+    def test_patch_bad_request(self):
+        """投稿モデルの登録APIへのPATCHリクエスト（異常系：バリデーションNG）"""
+
+        # APIリクエストを実行
+        post = Post.objects.create(
+            category=self.category1,
+            author=self.user,
+            title='へのへのもへじ',
+            content='あいうえおかきくけこ',
+        )
+
+        params = {
+            'category': self.category1.id,
+            'author_name': self.user.id,
+            'title': '',
+            'content': 'かきくけこ',
+        }
+        response = self.client.patch(self.TARGET_URL_WITH_PK.format(
+            post.id), params, format='json')
+
+        # データベースの状態を検証
+        # self.assertEqual(Post.objects.count(), 1)
         # レスポンスの内容を検証
         self.assertEqual(response.status_code, 400)
